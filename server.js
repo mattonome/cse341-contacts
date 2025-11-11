44b770aefdf29e79e9735c791f04e4f5080e90e7
mattonome <mattj4all@yahoo.com>
2025-11-11 11:08:16 +0100

    initial 2

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', (req, res) => {
   res.send('Welcome to my Express server with MongoDB!');
 });
 
-// Contacts routes nnn 
+// Contacts routes
 app.use('/contacts', require('./routes/contacts'));
 
 db.initDb((err) => {
